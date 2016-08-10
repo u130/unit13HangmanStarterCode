0cb47622e576137250b719069047811b78f5e99c
Bijesse <tom@bijesse.com>
2016-08-10 11:23:13 -0400

    added starter code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# unit13HangmanStarterCode
+# ScriptEd Hangman 
+
+This game will be created during unit 13 of the ScirptEd Foundations course. This project has been broken down into 5 steps in order to complete this game during ScriptEd.
+
+All of the HTML and CSS has been provided to you in the project. However, you must complete the script.js file on your own.
+
+## Hangman Steps
+**Step 1:** Preparing the game  
+**Step 2:**  Key Clicks (displaying what the user types in)  
+**Step 3:**  Evaluating (does the letter the user guessed match any letter in the word?)  
+**Step 4:**  Draw hangman (if user is wrong add body part)  
+**Step 5:**  Check if the user has won or lost
---
+++ script.js
@@ -0,0 +1,23 @@
+//Step 1 TASK: create a variable called secretWord and set it to null
+
+// When this function is called the game is staged to be played.
+function prepareGame() {
+// defines the secret word
+  secretWord = ['J','A','V','A','S','C','R', 'I', 'P', 'T'];
+
+// Step 1 TASK: call the drawWord and drawHangman function here (inside the prepareGame function).   
+
+}
+
+// This function draws the correct amount of underscores under hangman depending on the length of the word.
+function drawWord() {
+}
+
+// This function draws the hangman images depending on the number of wrong guesses there are.
+function drawHangman() {
+}
+
+$(document).ready(function() {
+// Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
+
+});
