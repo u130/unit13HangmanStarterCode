ae821b1ab050133203d0ebfe91f09631a53f7f26
Peter Jablonski <pwjablonski@gmail.com>
2017-04-28 13:01:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,139 @@
-//Step 1 TASK: create a variable called secretWord and set it to null
+//On the lines below define four variables secretWord, guesses, misses, and images
+// Set secretWord equal to null
+// Set correctGuesses equal to an empty array
+// Set wrongGuesses equal to an empty array
+// Set images to an array contataining all of your images links as strings (in order)
+var secretWord = null;
 
-// When this function is called the game is staged to be played.
+
+
+
+
+
+// in the prepareGame() function below
+// set correctGuesses to an empty array
+// set wrongGuesses to an empty array
+// call the drawWord() function
+// call the drawHangman() function
 function prepareGame() {
-// defines the secret word
   secretWord = ['J','A','V','A','S','C','R', 'I', 'P', 'T'];
 
-// Step 1 TASK: call the drawWord and drawHangman function here (inside the prepareGame function).   
-
+    
+  
 }
 
-// This function draws the correct amount of underscores under hangman depending on the length of the word.
+// in this onWin() function below 
+// 1. alert "You won!"
+function onWin() {
+  
+  
+}
+
+// in this onLose() function below 
+// 1. alert "You lost!"
+function onLose() {
+  
+  
+  
+}
+
+// in this checkIfWon() function below
+// 1. Declare a variable hasAll and set equal to true
+// 2. For each letter in secretWord 
+//    a. if correctGuesses does not include the letter (!) set variable hasAll to false
+// 3. return hasAll
+function checkIfWon() {  
+  
+  
+  
+}
+
+// in this checkIfLost() function below
+// 1. declare a variable misses and set it equal to the length of wrongGuesses array
+// 2. if misses is less than 6 return false else return true
+function checkIfLost() {
+  
+  
+  
+  
+}
+
+// in the onCorrectGuess() function below
+// 1. add the the letter variable to the array correctGuesses
+// 2. call the drawWord function
+// 3. if the checkIfWon() is returns true call the onWin() function 
+function onCorrectGuess(letter) {
+    
+  
+  
+  
+  
+}
+
+// in the onWrongGuess() function below
+// 1. add the the letter variable to the array wrongGuesses
+// 2. call the drawHangman function
+// 3. if the checkIfLost() function returns true call the onLose() function 
+function onWrongGuess(letter) {
+    
+  
+  
+  
+  
+}
+
+// in the judgeGuess function below
+// 1. if the letter is included in secretWord, call the onCorrectGuess(letter) function
+//    otherwise call onWrongGuess(letter) function
+function judgeGuess(letter) {
+  
+  
+  
+  
+  
+}
+
+// in the drawWord function below
+// 1. empty the div tag with the id "word"
+// 2. For each letter in secretWord 
+//    if correctGuesses includes letter append the letter
+//    othewise append and underscore
 function drawWord() {
+  
+  
+  
+  
+  
+  
 }
 
-// This function draws the hangman images depending on the number of wrong guesses there are.
+// in the drawHangman function below
+// 1. define a variable misses equal to the length of wrongGuesses
+// 2. change the src of the img tag with the id hangman 
+//    to the correct image url based on the number of misses
 function drawHangman() {
+  
+  
+  
+  
 }
 
-$(document).ready(function() {
-// Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
+// in the onKeyDown function below
+// 1. define a variable letter an set it equal to the correct letter
+// 2. set letter equal to the upperCase of itself
+// 3. call the judgeGuess function with letter as an argument
 
+function onKeyDown(event) {
+  
+  
+  
+  
+}
+
+// Call the prepare game function
+// Initialize a jQuery keydown event handler 
+//       (Keydown function should take onKeyDown function as an argument)
+$(document).ready(function() {
+ 
+  
 });
